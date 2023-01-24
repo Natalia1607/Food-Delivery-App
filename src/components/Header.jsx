6abc25b776588336d2b4788b32d1b7cf02a63c59
Natalia Gilyazova <gilyazovan97@mail.ru>
2023-01-24 22:11:14 +0300

    Added images

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,4 +1,5 @@
-import React from 'react'
+import React from 'react';
+import Logo from '../assets/img/logo.png';
 
 const Header = () => {
   return (
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,93 +1,61 @@
 module.exports = {
-  purge: ['./src/**/*.{js,jsx,ts,tsx}', './public/index.html'],
-  darkMode: false, // or 'media' or 'class'
+  content: ["./src/**/*.{js,jsx,ts,tsx}"],
   theme: {
     extend: {
-      margin: {
-        320: '320px',
-      },
       width: {
-        190: '190px',
-        275: '275px',
-        300: '300px',
-        340: '340px',
-        350: '350px',
-        656: '656px',
-        880: '880px',
-        508: '508px',
+        150: "150px",
+        190: "190px",
+        225: "225px",
+        275: "275px",
+        300: "300px",
+        340: "340px",
+        350: "350px",
+        375: "375px",
+        460: "460px",
+        656: "656px",
+        880: "880px",
+        508: "508px",
       },
       height: {
-        80: '80px',
-        340: '340px',
-        370: '370px',
-        420: '420px',
-        510: '510px',
-        600: '600px',
-        685: '685px',
-        800: '800px',
-        '90vh': '90vh',
-      },
-      flex: {
-        0.7: '0.7 1 0%',
-      },
-      maxHeight: {
-        370: '370px',
+        80: "80px",
+        150: "150px",
+        225: "225px",
+        300: "300px",
+        340: "340px",
+        370: "370px",
+        420: "420px",
+        510: "510px",
+        600: "600px",
+        650: "650px",
+        685: "685px",
+        800: "800px",
+        "90vh": "90vh",
       },
       minWidth: {
-        210: '210px',
-        350: '350px',
-        620: '620px',
+        210: "210px",
+        350: "350px",
+        620: "620px",
       },
-      textColor: {
-        lightGray: '#F1EFEE',
-        primary: '#FAFAFA',
-        secColor: '#efefef',
-        navColor: '#BEBEBE',
+      screens: {
+        sm: "640px",
+        md: "768px",
+        lg: "1024px",
+        xl: "1280px",
+        "2xl": "1536px",
       },
-      backgroundColor: {
-        mainColor: '#FBF8F9',
-        secondaryColor: '#F0F0F0',
-        blackOverlay: 'rgba(0, 0 ,0 ,0.7)',
-      },
-      keyframes: {
-        'slide-in': {
-          '0%': {
-            '-webkit-transform': 'translateX(-200px)',
-            transform: 'translateX(-200px)',
-          },
-          '100%': {
-            '-webkit-transform': 'translateX(0px)',
-            transform: 'translateX(0px)',
-          },
-        },
-
-        'slide-fwd': {
-          '0%': {
-            '-webkit-transform': 'translateZ(0px)',
-            transform: 'translateZ(0px)',
-          },
-          '100%': {
-            '-webkit-transform': 'translateZ(160px)',
-            transform: 'translateZ(160px)',
-          },
-        },
-      },
-      animation: {
-        'slide-in': 'slide-in 0.5s ease-out',
-        'slide-fwd': ' slide-fwd 0.45s cubic-bezier(0.250, 0.460, 0.450, 0.940) both',
-      },
-      transitionProperty: {
-        height: 'height',
+      colors: {
+        headingColor: "#2e2e2e",
+        textColor: "#515151",
+        cartNumBg: "#e80013",
+        primary: "#f5f3f3",
+        cardOverlay: "rgba(256,256,256,0.4)",
+        lighttextGray: "#9ca0ab",
+        card: "rgba(256,256,256,0.8)",
+        cartBg: "#282a2c",
+        cartItem: "#2e3033",
+        cartTotal: "#343739",
       },
     },
-    cursor: {
-      'zoom-in': 'zoom-in',
-      pointer: 'pointer',
-    },
   },
-  variants: {
-    // backgroundColor: ['active'],
-    extend: {},
-  },
-  plugins: [],
+  plugins: [require("tailwind-scrollbar")],
 };
