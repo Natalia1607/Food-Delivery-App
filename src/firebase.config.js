c9815c024c182b79cc5e389ca41605f64e750528
Natalia Gilyazova <gilyazovan97@mail.ru>
2023-04-14 08:01:07 +0300

    Firebase create item

--- src/firebase.config.js
+++ src/firebase.config.js
@@ -5,11 +5,12 @@ import { getStorage } from 'firebase/storage';
 const firebaseConfig = {
     apiKey: "AIzaSyCXKL4XTTA7CmnzOz40Tw5FBiA9z1X_gew",
     authDomain: "restaurantapp-ba0ca.firebaseapp.com",
+    databaseURL: "https://restaurantapp-ba0ca-default-rtdb.firebaseio.com",
     projectId: "restaurantapp-ba0ca",
     storageBucket: "restaurantapp-ba0ca.appspot.com",
     messagingSenderId: "1079195330361",
     appId: "1:1079195330361:web:40aaddfe29e4cb736ceae8"
-};
+  };
 
 const app = getApps.length > 0 ? getApp() : initializeApp(firebaseConfig);
 const firestore = getFirestore(app);
